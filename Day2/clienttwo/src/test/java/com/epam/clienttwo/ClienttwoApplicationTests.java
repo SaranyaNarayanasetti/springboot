6b01c1b8b4be3e9214e0a3fe7c0c18f970eff200
SaranyaNarayanasetti <narayanasettisaranya@gmail.com>
2024-11-20 12:20:09 +0530

    Api Gateway And Routing its Predicates

---
+++ Day2/clienttwo/src/test/java/com/epam/clienttwo/ClienttwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.clienttwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienttwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
